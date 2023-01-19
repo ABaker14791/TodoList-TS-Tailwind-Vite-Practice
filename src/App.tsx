f77dce714f5be340a80a4f21a813992781b78108
Adam Baker <adam_baker@live.co.uk>
2023-01-19 21:53:16 +0000

    Feature: Added date and time task was posted

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
 	const input = useRef<HTMLInputElement>(null);
-	const [todoList, setTodoList] = useState<{ id: number; taskName: string }[]>(
-		[]
-	);
+	const [todoList, setTodoList] = useState<
+		{ id: number; taskName: string; date: string; time: string }[]
+	>([]);
 	const [newTask, setNewTask] = useState<string>("");
 
 	function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -16,10 +16,16 @@ function App() {
 		event.preventDefault();
 
 		if (newTask) {
-			const task: { id: number; taskName: string } = {
-				id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-				taskName: newTask,
-			};
+			const task: { id: number; taskName: string; date: string; time: string } =
+				{
+					id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+					taskName: newTask,
+					date: new Date().toLocaleDateString(),
+					time: new Date().toLocaleTimeString("en-GB", {
+						hour: "numeric",
+						minute: "numeric",
+					}),
+				};
 			setTodoList([...todoList, task]);
 			if (input.current !== null) {
 				input.current.value = "";
@@ -57,7 +63,15 @@ function App() {
 							key={todo.id}
 							className="flex gap-4 mt-2 pl-4 justify-between border-2 rounded-lg"
 						>
-							<li className="py-2 text-xl">{todo.taskName}</li>
+							<li className="py-2 text-xl w-full">
+								<div className="">
+									<div className="flex justify-between w-full">
+										<div className="text-slate-500 text-sm">{todo.date}</div>
+										<div className="text-slate-500 text-sm">{todo.time}</div>
+									</div>
+									<div className="">{todo.taskName}</div>
+								</div>
+							</li>
 							<button
 								onClick={() => deleteTask(todo)}
 								className="text-red-700 bg-zinc-300 px-4 rounded-tr rounded-br cursor-pointer"
